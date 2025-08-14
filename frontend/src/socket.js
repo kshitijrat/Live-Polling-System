57a262b772cdc8c1c36e37bd4c2972f13adb2ee9
kshitijrat <kshitijratnawat@gmail.com>
2025-08-14 19:30:06 +0530

    update

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://polling-system-backend-t17a.onrender.com"); 
+const socket = io("https://live-polling-system-5f56.onrender.com"); 
 
 export default socket;
